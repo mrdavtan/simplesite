cec2b3650ca93135d2b02f5f9544cdfb47280ee3
mrdavtan <mrdavtan@protonmail.com>
2024-02-27 01:19:39 -0800

    auth0 login working

--- server.js
+++ server.js
@@ -13,6 +13,37 @@ import { v4 as uuidv4 } from 'uuid';
 const app = express();
 const PORT = 3001;
 
+
+//auth0
+import { auth } from 'express-openid-connect';
+
+
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3001',
+  clientID: 'OOALgl4ZmNpRpQxcFi81jGjbglgA23xI',
+  issuerBaseURL: 'https://dev-wagoz1ws8eq4zztu.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
+//user profile info
+//app.get('/profile', requiresAuth(), (req, res) => {
+//  res.send(JSON.stringify(req.oidc.user));
+//});
+
+
+
 // Initialize OpenAI - Ensure you replace with your actual initialization
 const openai = new OpenAI();
 
