e328c433d28beac5158e99b8907e8f200e4298f7
mrdavtan <mrdavtan@protonmail.com>
2024-02-27 08:47:06 -0800

    removed unused

--- vcs/tracker.py
+++
@@ -1,9 +0,0 @@
-from .content import store_object, hash_content
-
-# tracker.py
-def track_changes(repo_name='.yourvcs'):
-    """Track file changes in the working directory."""
-    # This function should compare the current state of files to the last commit
-    # For simplicity, this example does not include the full implementation
-    pass
-
